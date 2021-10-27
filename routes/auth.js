8ea72801e3971223ab2345a4b190145982aa3847
Mauro Quirinali <mquirina@boldt.com.ar>
2021-10-27 13:49:55 -0300

    Fin sección 12

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@
 */
 
 const { Router } = require('express');
-const { login } = require('../controllers/auth');
+const { login, googleSingIn } = require('../controllers/auth');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
@@ -17,5 +17,11 @@ router.post('/', [
     login
 )
 
+router.post('/google', [
+    check('token', 'El token de Google es obligatorio').not().isEmpty(),    
+    validarCampos,
+    ],
+    googleSingIn
+)
 
 module.exports = router;
